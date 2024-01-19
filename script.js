02a0c060eaa202778e408a42b940b91bc9ff98a3
Lavanya2oo4 <lvsingh2004@gmail.com>
2024-01-19 14:52:48 -0500

    Added functionality to Print Health Issues by taking user input

--- script.js
+++ script.js
@@ -5,7 +5,14 @@ let form = document.getElementById("form").addEventListener("submit", (e) => {
 
 
 
+
+
+
+
+
 async function displayAll() {
+    let output = document.getElementById("output")
+    output.innerHTML = ""
 
 
     //getting data
@@ -17,7 +24,6 @@ async function displayAll() {
 
     let iteration = 0
     dataParsed.forEach(element => {
-        let output = document.getElementById("output")
 
 
         //to print the heading
@@ -34,15 +40,85 @@ async function displayAll() {
         output.append(`${element.name}`)
 
         // to print a line break
-        let break_= document.createElement("br")
-      output.appendChild(break_)
-
-
-
-
-        console.log(element.name)
+        let break_ = document.createElement("br")
+        output.appendChild(break_)
     });
 
 
 
+}
+
+
+
+async function displayHealthIssues() {
+
+    let input = document.getElementById("healthIssue").value
+
+    let output = document.getElementById("output")
+    output.innerHTML = ""
+
+    if(input==""){
+
+        output.append("⚠️Enter Valid Data")
+        return
+    }
+
+
+
+
+
+    //getting data
+
+    let url = "https://api.thecatapi.com/v1/breeds"
+    let data = await fetch(url)
+    let dataParsed = await data.json()
+
+
+    let iteration = 0
+
+    //printing total Health results data
+    let totalResults = 0
+
+
+    dataParsed.forEach(element => {
+
+
+
+        //to print the heading
+        if (iteration == 0) {
+            let dataHead = document.createElement("h3")
+            dataHead.innerHTML = `Health Issues For :${input}`
+            output.appendChild(dataHead)
+        }
+
+        iteration += 1
+
+
+
+
+
+        if (element.health_issues == input) {
+            output.append(`${element.name}`)
+
+
+            // to print a line break
+            let break_ = document.createElement("br")
+            output.appendChild(break_)
+            totalResults += 1
+
+
+
+        }
+        else {
+            return
+        }
+
+
+    });
+
+    let dataTotal = document.createElement("h4")
+    dataTotal.innerHTML = `Total Results :${totalResults}`
+    output.appendChild(dataTotal)
+    
+    console.log(totalResults)
 }
